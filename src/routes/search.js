05e01a9f1b1c2db089256602954690745221e18e
takshch <takshch089@gmail.com>
2022-06-04 17:10:06 +0530

    Changes /search/suggestion to search/suggestions

--- src/routes/search.js
+++ src/routes/search.js
@@ -5,7 +5,7 @@ const validator = require('../validators/search');
 const searchRouter = express.Router();
 
 searchRouter.post('/', validator.search, controller.search);
-searchRouter.get('/suggestion', validator.suggestion, controller.suggestion);
+searchRouter.get('/suggestions', validator.suggestion, controller.suggestion);
 
 module.exports = { searchRouter };
 
